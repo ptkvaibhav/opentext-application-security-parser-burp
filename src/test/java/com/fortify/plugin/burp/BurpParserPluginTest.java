b139bc5bca92dda992e53eb80d43961fc6229fde
Pratik Vaibhav <ptkvaibhav@gmail.com>
2026-04-06 15:27:45 +0530

    chore: make repository production-ready

--- src/test/java/com/fortify/plugin/burp/BurpParserPluginTest.java
+++ src/test/java/com/fortify/plugin/burp/BurpParserPluginTest.java
@@ -16,7 +16,9 @@ import java.util.function.Predicate;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class BurpParserPluginTest {
